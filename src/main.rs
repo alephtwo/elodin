d9770f0dfe3bffe3089ea20ad710b484436ccf07
Ben Christianson <thhuntertgm@gmail.com>
2018-03-23 13:36:47 -0500

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate clap;
 extern crate inflector;
 extern crate rand;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 use inflector::Inflector;
 use rand::{seq, thread_rng};
 
@@ -10,30 +10,37 @@ fn main() {
     let matches = App::new("elodin")
         .version("0.3.0")
         .author("Ben Christianson <thhuntertgm@gmail.com>")
-        .arg(Arg::with_name("slug")
-            .short("s")
-            .help("Generate a slug instead of a name"))
+        .arg(
+            Arg::with_name("slug")
+                .short("s")
+                .help("Generate a slug instead of a name"),
+        )
         .get_matches();
-
 
     let slug = matches.is_present("slug");
     let joiner = if slug { "-" } else { " " };
 
     let words: Vec<String> = generate()
         .iter()
-        .map(|w| if slug { w.to_lowercase() } else { w.to_title_case() })
+        .map(|w| {
+            if slug {
+                w.to_lowercase()
+            } else {
+                w.to_title_case()
+            }
+        })
         .collect();
 
     println!("{}", words.join(joiner))
 }
 
-fn generate () -> Vec<&'static str> {
+fn generate() -> Vec<&'static str> {
     let noun = choose_word_from_file(include_str!("nouns.txt"));
     let adjective = choose_word_from_file(include_str!("adjectives.txt"));
     vec![noun, adjective]
 }
 
-fn choose_word_from_file (text: &str) -> &str {
+fn choose_word_from_file(text: &str) -> &str {
     let words = text.trim().split("\n").collect();
     choose_random_word(words)
 }
