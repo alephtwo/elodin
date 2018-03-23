e1a62f7fded791b53b5cde4059383fb44a2f9570
Ben Christianson <thhuntertgm@gmail.com>
2018-03-23 10:09:33 -0500

    Rust works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate rand;
+extern crate inflector;
+use rand::{thread_rng, seq};
+use inflector::Inflector;
+
 fn main() {
-    println!("Hello, world!");
+    let nouns = parse_text(include_str!("nouns.txt"));
+    let adjectives = parse_text(include_str!("adjectives.txt"));
+
+    let noun = choose_word(nouns).to_title_case();
+    let adjective = choose_word(adjectives).to_title_case();
+
+    println!("{} {}", adjective, noun);
+}
+
+fn parse_text(text: &str) -> Vec<&str> {
+    text.trim().split("\n").collect()
+}
+
+fn choose_word(words: Vec<&str>) -> &str {
+    let mut rng = thread_rng();
+    seq::sample_iter(&mut rng, words, 1).unwrap()[0]
 }
