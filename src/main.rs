fee817d22358b82e9b880c5e661b9adc4b4c46f9
Ben Christianson <thhuntertgm@gmail.com>
2018-03-23 13:00:54 -0500

    Refactor to remove duplication

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
+extern crate clap;
 extern crate inflector;
 extern crate rand;
-use rand::{seq, thread_rng};
+
+use clap::{Arg, App};
 use inflector::Inflector;
+use rand::{seq, thread_rng};
 
 fn main() {
-    let noun = generate_word(include_str!("nouns.txt"));
-    let adjective = generate_word(include_str!("adjectives.txt"));
-    println!("{} {}", adjective, noun);
+    let matches = App::new("elodin")
+        .version("0.3.0")
+        .author("Ben Christianson <thhuntertgm@gmail.com>")
+        .arg(Arg::with_name("slug")
+            .short("s")
+            .help("Generate a slug instead of a name"))
+        .get_matches();
+
+
+    let slug = matches.is_present("slug");
+    let joiner = if slug { "-" } else { " " };
+
+    let words: Vec<String> = generate()
+        .iter()
+        .map(|w| if slug { w.to_lowercase() } else { w.to_title_case() })
+        .collect();
+
+    println!("{}", words.join(joiner))
 }
 
-fn generate_word (text: &str) -> String {
+fn generate () -> Vec<&'static str> {
+    let noun = choose_word_from_file(include_str!("nouns.txt"));
+    let adjective = choose_word_from_file(include_str!("adjectives.txt"));
+    vec![noun, adjective]
+}
+
+fn choose_word_from_file (text: &str) -> &str {
     let words = text.trim().split("\n").collect();
-    choose_random_word(words).to_title_case()
+    choose_random_word(words)
 }
 
 fn choose_random_word(words: Vec<&str>) -> &str {
