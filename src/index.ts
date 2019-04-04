69bb48fa5533f9137c591ec26750c64975d54866
Ben Christianson <thhuntertgm@gmail.com>
2019-04-04 13:57:32 -0500

    Use semicolons in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import './index.css'
-import * as _ from 'lodash'
-import * as changeCase from 'change-case'
-import { Clipboard } from 'ts-clipboard'
-import * as nouns from './words/nouns.json'
-import * as adjectives from './words/adjectives.json'
+import './index.css';
+import * as _ from 'lodash';
+import * as changeCase from 'change-case';
+import { Clipboard } from 'ts-clipboard';
+import * as nouns from './words/nouns.json';
+import * as adjectives from './words/adjectives.json';
 
 declare global {
-  interface Window { generate: Function }
+  interface Window { generate: Function; }
 }
 
 window.generate = () => {
-  const word = makeWord()
-  Clipboard.copy(word)
+  const word = makeWord();
+  Clipboard.copy(word);
 
-  const button = document.getElementById('generate-button')
+  const button = document.getElementById('generate-button');
   if (button) {
-    button.textContent = word
+    button.textContent = word;
   }
-}
+};
 
 const makeWord = (): string => [adjectives, nouns]
   .map((list: Array<string>) => _.sample(list))
